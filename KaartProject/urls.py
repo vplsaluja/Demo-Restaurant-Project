a6681987f79d6a60ba76bb2025d39315e6522df5
Vipul Saluja <vplsaluja@gmail.com>
2018-07-23 18:43:16 +0530

    custom css

--- KaartProject/urls.py
+++ KaartProject/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url, include
+from django.contrib import admin
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
